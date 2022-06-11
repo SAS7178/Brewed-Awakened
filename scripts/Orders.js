a08390daa01328587aede8485c5630ff9ab9f260
Stephen Smith <StephenASmith7178@gmail.com>
2022-06-11 14:38:13 -0400

    final push

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -8,7 +8,7 @@ const orders = getOrders()
 
 // Function whose responsibility is to find the product for an order
 const findproduct = (order, allProducts) => {
-    let orderProduct = {}
+    let orderProduct = null
 
     for (const product of allProducts) {
         if (product.id === order.productId) {
@@ -21,29 +21,29 @@ const findproduct = (order, allProducts) => {
 
 // Function whose responsibility is to find the employee for an order
 const findemployee = (order, allEmployees) => {
-    let orderEmployee = {}
+    let orderEmployee = null
 
     for (const employee in allEmployees) {
-        if (employee.id === order.employeeId) {
-            orderEmployee = employee
-        }
+        if (allEmployees[employee].id === order.employeeId) {
+            orderEmployee = allEmployees[employee]
+        }   
     }
-
     return orderEmployee
-}
+} 
+
 
 export const Orders = () => {
     let html = ""
-    html = "<ul>"
+    html += "<ul>"
 
 
     for (const order of orders) {
         const employee = findemployee(order,employees)
         const product = findproduct(order,products)
         
-
-        html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
-    }
+        if(product !== null && employee !== null)
+       { html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+    }}
 
     html += "</ul>"
 
